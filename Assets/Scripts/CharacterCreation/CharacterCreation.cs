44572cb067a670a35367e3386114a4a48e786b41
BDeWinne <68663886+BDeWinne@users.noreply.github.com>
2022-10-22 15:21:29 -0300

    character creation without conection with gameplay

---
+++ Assets/Scripts/CharacterCreation/CharacterCreation.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+public class CharacterCreation : MonoBehaviour
+{
+    [SerializeField] GameObject[] headPrefabs;
+    int headIndex = 0;
+    [SerializeField] GameObject[] clothesPrefabs;
+    int clothesIndex = 0;
+    [SerializeField] Material[] tones;
+    int tonesIndex = 0;
+    [SerializeField] MeshRenderer playerMesh;
+    void Start()
+    {
+        if (File.Exists(Application.persistentDataPath + "/playerIndexes"))
+        {
+            InitialLoad();
+        }
+    }
+    void InitialLoad()
+    {
+        List<int> initialIndexes = new List<int>();
+        initialIndexes = FileHandler.ReadListFromJSON<int>("playerIndexes");
+        headIndex = initialIndexes[0];
+        clothesIndex = initialIndexes[1];
+        tonesIndex = initialIndexes[2];
+        headSelector(0);
+        clothesSelector(0);
+        toneSelector(0);
+    }
+    public void headSelector(int value)
+    {
+        headPrefabs[headIndex].SetActive(false);
+        headIndex += value;
+        if (headIndex > headPrefabs.Length - 1)
+        {
+            headIndex = 0;
+        }
+        if (headIndex == -1)
+        {
+            headIndex = headPrefabs.Length - 1;
+        }
+        headPrefabs[headIndex].SetActive(true);
+    }
+    public void clothesSelector(int value)
+    {
+        clothesPrefabs[clothesIndex].SetActive(false);
+        clothesIndex += value;
+        if (clothesIndex > clothesPrefabs.Length - 1)
+        {
+            clothesIndex = 0;
+        }
+        if (clothesIndex == -1)
+        {
+            clothesIndex = clothesPrefabs.Length - 1;
+        }
+        clothesPrefabs[clothesIndex].SetActive(true);
+    }
+    public void toneSelector(int value)
+    {
+        tonesIndex += value;
+        if (tonesIndex > tones.Length - 1)
+        {
+            tonesIndex = 0;
+        }
+        if (tonesIndex == -1)
+        {
+            tonesIndex = tones.Length - 1;
+        }
+        playerMesh.material = tones[tonesIndex];
+    }
+    public void CreateListWithIndexes()
+    {
+        List<int> indexes = new List<int>();
+        indexes.AddRange(new List<int>{
+            headIndex,
+            clothesIndex,
+            tonesIndex,
+        });
+        FileHandler.SaveToJSON(indexes, "playerIndexes");
+    }
+}
---
+++ Assets/Scripts/SceneChanger/ScenesChanger.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ScenesChanger : MonoBehaviour
+{
+    public void SceneChange(string sceneName)
+    {
+        StartCoroutine(LoadAsyncScene(sceneName));
+    }
+    IEnumerator LoadAsyncScene(string scene)
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(scene);
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+}
