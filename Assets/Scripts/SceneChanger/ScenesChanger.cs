6a1895fc94f900f60a089d7cfbaaf4e98a1a3f8b
BDeWinne <68663886+BDeWinne@users.noreply.github.com>
2022-11-13 21:35:21 -0300

    main menu added and other changes

--- Assets/Scripts/SceneChanger/ScenesChanger.cs
+++ Assets/Scripts/SceneChanger/ScenesChanger.cs
@@ -4,17 +4,23 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class ScenesChanger : MonoBehaviour
 {
+    public static ScenesChanger SharedInstance;
     void Awake()
     {
         DontDestroyOnLoad(gameObject);
+        SharedInstance = this;
+    }
+    public void AddScene(string sceneName)
+    {
+        LoadAsyncScene(sceneName, LoadSceneMode.Additive);
     }
     public void SceneChange(string sceneName)
     {
-        StartCoroutine(LoadAsyncScene(sceneName));
+        StartCoroutine(LoadAsyncScene(sceneName, LoadSceneMode.Single));
     }
-    IEnumerator LoadAsyncScene(string scene)
+    IEnumerator LoadAsyncScene(string scene, LoadSceneMode loadSceneMode)
     {
-        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(scene);
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(scene, loadSceneMode);
         while (!asyncLoad.isDone)
         {
             yield return null;
---
+++ Assets/Scripts/UI/MainMenu/MainMenu_Manager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu_Manager : MonoBehaviour
+{
+    [SerializeField] GameObject mainMenuPanel;
+    [SerializeField] GameObject confirmationPanel;
+    [SerializeField] GameObject creditsPanel;
+    public void EnterGame(string sceneName)
+    {
+        ScenesChanger.SharedInstance.SceneChange(sceneName);
+    }
+    public void MainMenuPanel(bool state)
+    {
+        mainMenuPanel.SetActive(state);
+    }
+    public void CreditsPanel(bool state)
+    {
+        creditsPanel.SetActive(state);
+    }
+    public void ConfirmationPanel(bool state)
+    {
+        confirmationPanel.SetActive(state);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
